2dbc439f229f743c84ed85ccab82823ef512c860
Siarhei Vishniakou <svv@google.com>
2018-06-28 10:53:18 +0100

    input: touchscreen: Require low latency
    Currently, CPU can enter deep sleep while waiting for data on the i2c
    bug. That means an i2c read of 8 bytes can take as long as 10 ms, which
    would overrun the desired interrupt handling time.
    Use pm qos to require low latency and prevent CPU from entering deep
    sleep while handling touch interrupts.
    
    Bug: 110939384
    Test: look at the i2c read traces:
    1) ./external/chromium-trace/systrace.py --atrace-categories=view,wm,am,app,gfx,i2c,sched,irq,video,power,input,workq,freq
    2) perform a fling
    3) review the i2c read duration on the touchscreen bus
    
    Change-Id: Icf8ab09324003a85af70517769ced3bae52f705c
    Signed-Off-By: Siarhei Vishniakou <svv@google.com>

--- drivers/input/touchscreen/stm/fts_ts.c
+++ drivers/input/touchscreen/stm/fts_ts.c
@@ -2258,6 +2258,9 @@ static irqreturn_t fts_interrupt_handler(int irq, void *handle)
 		/* run lpm interrupt handler */
 	}
 
+	/* prevent CPU from entering deep sleep */
+	pm_qos_update_request(&info->pm_qos_req, 100);
+
 	evtcount = 0;
 	fts_read_reg(info, &regAdd[0], 3, (unsigned char *)&evtcount, 2);
 
@@ -2273,6 +2276,8 @@ static irqreturn_t fts_interrupt_handler(int irq, void *handle)
 				  FTS_EVENT_SIZE * evtcount);
 		fts_event_handler_type_b(info, info->data, evtcount);
 	}
+
+	pm_qos_update_request(&info->pm_qos_req, PM_QOS_DEFAULT_VALUE);
 
 	return IRQ_HANDLED;
 }
@@ -2930,6 +2935,9 @@ static int fts_probe(struct i2c_client *client, const struct i2c_device_id *idp)
 		info->finger[i].mcount = 0;
 	}
 
+	pm_qos_add_request(&info->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
+			PM_QOS_DEFAULT_VALUE);
+
 	retval = fts_irq_enable(info, true);
 	if (retval < 0) {
 		input_err(true, &info->client->dev,
@@ -3050,6 +3058,7 @@ err_sec_cmd:
 	tui_tsp_info = NULL;
 #endif	
 err_enable_irq:
+	pm_qos_remove_request(&info->pm_qos_req);
 	if (info->board->support_dex) {
 		input_unregister_device(info->input_dev_pad);
 		info->input_dev_pad = NULL;
@@ -3166,6 +3175,8 @@ static int fts_remove(struct i2c_client *client)
 #endif
 	info->shutdown_is_on_going = false;
 	kfree(info);
+
+	pm_qos_remove_request(&info->pm_qos_req);
 
 	return 0;
 }
